02b5fdf5856dd980879dfa216707617981aced59
MSI\marco <marcocarega.dev@gmail.com>
2024-03-31 20:37:57 +0200

    app almost finished

--- lib/screens/MainScreen.dart
+++ lib/screens/MainScreen.dart
@@ -1,5 +1,7 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:dndify/widgets/AudioGridTile.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 class MainScreen extends StatefulWidget
 {
@@ -14,29 +16,48 @@ class MainScreenState extends State<MainScreen>
 {
   List<AudioGridTile> tiles=[];
 
+  final AudioPlayer player=AudioPlayer();
+
+  Function() onTap(Source source)
+  {
+    return ()
+    {
+      player.stop();
+      player.play(source);
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<FileSystemEntity> files=readFiles();
+    tiles=files.map<AudioGridTile>(mappingFunction).toList();
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("DnDify",style: TextStyle(color: Colors.white),),
-        backgroundColor: Colors.redAccent
+        title: Text("DnDify",style: TextStyle(color: Theme.of(context).colorScheme.background),),
+        backgroundColor: Theme.of(context).colorScheme.primary
       ),
       body: makeGridView(),
     );
+  }
+
+  AudioGridTile mappingFunction(FileSystemEntity file)
+  {
+    DeviceFileSource source=DeviceFileSource(file.path);
+    return AudioGridTile(audio: source, onTap: onTap);
+  }
+
+  List<FileSystemEntity> readFiles()
+  {
+    Directory dir=Directory("assets/audio/");
+    return dir.listSync();
   }
 
   GridView makeGridView()
   {
     return GridView.count(
       crossAxisCount: 3,
-      children: [
-        AudioGridTile(),
-        AudioGridTile(),
-        AudioGridTile(),
-        AudioGridTile(),
-        AudioGridTile(),
-        AudioGridTile(),
-      ],
+      children: tiles
     );
   }
 }
--- lib/widgets/AudioGridTile.dart
+++ lib/widgets/AudioGridTile.dart
@@ -1,32 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class AudioGridTile extends StatelessWidget
 {
+  final DeviceFileSource audio;
 
+  final Function(Source) onTap;
+
+  AudioGridTile({required this.audio, required this.onTap})
+  {
+
+  }
 
   @override
   Widget build(BuildContext context)
   {
     return Card(
-        margin: EdgeInsets.all(2.0),
         child: InkWell(
-          onTap: onTap,
-            child: Container(
-                  child: LayoutBuilder(
-                    builder: (context,constraints)
-                    {
-                      return Icon(Icons.local_fire_department_rounded,color: Colors.redAccent,
-                        size: constraints.biggest.height/2);
-                    },
-                  )
-                )
-              ),
-        );
+          onTap: onTap(audio),
+          child: LayoutBuilder(builder: builderFunction(audio))
+        ),
+    );
   }
 
-  void onTap()
+  String formatText(String text)
   {
-    print("Tapped!");
+    text=audio.path;
+    List<String> parts=text.split("/");
+    text=parts[parts.length-1];
+    parts=text.split(".");
+    text=parts[0];
+    text=text.replaceAll("_"," ");
+    return text;
   }
+
+  Widget Function(BuildContext,BoxConstraints) builderFunction(DeviceFileSource audio)
+  {
+    double distance=5;
+    String text=formatText(audio.path);
+    return (BuildContext context,BoxConstraints constraints)
+    {
+      return Container(
+        margin: EdgeInsets.all(distance),
+        padding: EdgeInsets.all(distance),
+        child: Column(
+          children: [
+            Icon(Icons.local_fire_department_rounded,color: Theme.of(context).colorScheme.primary,
+                size: constraints.biggest.height/2),
+            Center(child: Text(text, style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  textBaseline: TextBaseline.ideographic
+                ),
+              )
+            )
+          ],
+        ),
+      );
+    };
+  }
+
 
 }
